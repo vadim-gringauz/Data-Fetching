8ef0c28bc67451e94f28272ab048b944f5447875
Vadim Gringauz <vadim.grin28@gmail.com>
2022-09-09 12:24:48 +0300

    bug in submit
    - sumbit doesnot work.

--- data.js
+++ data.js
@@ -5,34 +5,49 @@ const serverURL = "https://630f59da37925634188d7eb8.mockapi.io/form/list";
 let isEditOn = false;
 let editOpenID = 0;
 
-async function getData(url) {
-    console.log('loading');
-    // window.alert("loading");
-    // Loading spinner - ON
-    // document.getElementById("loading").hidden = false;
 
+/*********************************
+* GET ALL RECORDS FROM DATA
+* AND ADD EACH ONE TO THE HTML TABLE
+**********************************/
+async function getData(url) {
     try {
-        console.log('Done loading');
+        console.log('loading');
+        // window.alert("loading");
+        // Loading spinner - ON
+        document.getElementById("loading").hidden = false;
+        
         console.log('Server= ', url);
         console.log('getting data');
         const response = await fetch(url);
         const result = await response.json();
-        // console.log(result);
-        result.forEach((record) => {
-            // console.log('record:',index,'name= ', record.name);
+        result.forEach((record, index) => {
+            console.log('record:',index,'name= ', record.name);
             addRecordToPage(record);
         })
         
+        /* adds event listner only AFTER all 
+        * records were added to html        */
+       initAfterApendData();
+       const newRecForm = document.getElementById("add-record-form");
+       newRecForm.addEventListener("submit", submitNewRec);
+       
+       /* now adding event-listners for each record */  
+       
     } catch(err) {
         console.log('error:', err);
         window.alert("error retrieving data");
     } finally {
-        // Loading spinner - off!
-        // document.getElementById("loading").hidden = true;
+        /* Loading spinner - off! */
+        document.getElementById("loading").hidden = true;
+        console.log('Done loading');
     }
-    // console.log('after catch');
+    console.log('after catch');
 }
 
+/*********************************
+* ADD NEW RECORD FROM HTML TO DATA
+**********************************/
 async function sendPOSTRequest(url, body) {
     try {
         console.log('posting!!!');
@@ -54,6 +69,9 @@ async function sendPOSTRequest(url, body) {
     }
 }
 
+/*****************************
+*DELETS RECORD BY ID FROM DATA 
+******************************/
 function sendDELETEequest(idToDelete) {
     // const idToDelete = 11;
     console.log('delete id=', idToDelete,' was sent!!!');
@@ -77,6 +95,10 @@ function sendDELETEequest(idToDelete) {
     })
 }
 
+/*********************************
+* SENDS FETCH-PUT TO 'EDIT' RECORD
+*IN JSON ORIGIN
+*********************************/
 function sendPUTRequest(url, body) {
     console.log('putting!!!');
 
@@ -99,6 +121,9 @@ function sendPUTRequest(url, body) {
     })
 }
 
+/*******************
+*ADD RECORD TO HTML 
+********************/
 function addRecordToPage(record) {
     const id = record.id;
     const name = record.name;
@@ -126,140 +151,28 @@ function addRecordToPage(record) {
             </td>
             <td id="td-del-btn${id}">
                 <button type="button" id="delete-btn${id}" class="btn btn-danger btn-sm" onclick="sendDELETEequest(${id})">Delete</button>
-            </td>    
-            </td>
+            </td>  
         </tr>
     `;
 
     // Adding a new TR of the 'record' to the table using the template
-    const table = document.querySelector("table");
-    table.innerHTML += trTemplate;
-    table.append;
+    const tableBody = document.getElementById("table-body");
+    tableBody.innerHTML += trTemplate;
 
 
-    const newRecForm = document.getElementById("add-record");
+    // const newRecForm = document.getElementById("add-record");
     // newRecForm.addEventListener("submit", submitNewRec);
-    newRecForm.addEventListener("submit", myFunc);
+    // newRecForm.addEventListener("submit", myFunc);
 }
 
-
-// * old method for building each new TR *
-// function addRecordToPageOld(record) {
-//     // console.log('The record=', record);
-
-//     // Building TR + ID
-//     const tr = document.createElement("tr");
-//     tr.id = "tr" + record.id;
-
-//     const th = document.createElement("th");
-//     th.scope = "row";
-//     th.innerHTML = record.id;
-
-//     //  Building TD NAME
-//     const tdName = document.createElement("td");
-//     tdName.id = "td-name" + record.id;
-//     // div as text inside the TD = visible
-//     const nameText = document.createElement("div");
-//     nameText.id = "name-text" + record.id;
-//     nameText.innerHTML = record.name;
-//     // div as input inside the TD = hidden
-//     const nameInputDiv = document.createElement("div");
-//     nameInputDiv.id = "name-input" + record.id;
-//     nameInputDiv.hidden = true;
-//     // nameInputDiv.setAttribute("style", "width:5px");
-//     // input box inside the div
-//     const nameInputBox = document.createElement("input");
-//     nameInputBox.type = "text";
-//     nameInputBox.value = record.name;
-//     nameInputBox.className = "form-control";
-
-//     //  Building TD EMAIL
-//     const tdEmail = document.createElement("td");
-//     tdEmail.id = "td-email" + record.id;
-//     // div as text inside the TD = visible
-//     const emailText = document.createElement("div");
-//     emailText.id = "email-text" + record.id;
-//     emailText.innerHTML = record.email;
-//     // div as input inside the TD = hidden
-//     const emailInputDiv = document.createElement("div");
-//     emailInputDiv.id = "email-input" + record.id;
-//     emailInputDiv.hidden = true;
-//     // emailInputDiv.setAttribute("style", "width:5px");
-//     // input box inside the div
-//     const emailInputBox = document.createElement("input");
-//     emailInputBox.type = "text";
-//     emailInputBox.value = record.email;
-//     emailInputBox.className = "form-control";
-
-//     //  Building TD for save button
-//     const tdSaveBtn = document.createElement("td");
-//     tdSaveBtn.id = "td-save-btn" + record.id;
-//     const btnSave = document.createElement("button");
-//     btnSave.id = "save-btn" + record.id;
-//     btnSave.type = "button";
-//     btnSave.hidden = true;
-//     btnSave.className = "btn btn-success btn-sm";
-//     btnSave.innerHTML = "Save";
-
-//     //  Building TD for cancel button
-//     const tdCancelBtn = document.createElement("td");
-//     tdCancelBtn.id = "td-cancel-btn" + record.id;
-//     const btnCancel = document.createElement("button");
-//     btnCancel.id = "cancel-btn" + record.id;
-//     btnCancel.type = "button";
-//     btnCancel.hidden = true;
-//     btnCancel.className = "btn btn-warning btn-sm";
-//     btnCancel.innerHTML = "Cancel";
-
-//     //  Building TD for DELETE button
-//     const tdDelBtn = document.createElement("td");
-//     tdDelBtn.id = "td-del-btn" + record.id;
-//     const btnDelete = document.createElement("button");
-//     btnDelete.id = "delete-btn" + record.id;
-//     btnDelete.type = "button";
-//     btnDelete.className = "btn btn-danger btn-sm";
-//     btnDelete.innerHTML = "Delete";
-
-//     // Appending all objects:
-//     document.getElementById("table-body").appendChild(tr);
-//     const newAppendedTr = document.getElementById(tr.id);
-    
-//     newAppendedTr.appendChild(th);
-
-//     newAppendedTr.appendChild(tdName);
-//     document.getElementById(tdName.id).setAttribute( "onClick", "turnEditOn(" + record.id + ")");
-//     const appendedTdName = document.getElementById(tdName.id);
-//     appendedTdName.appendChild(nameText);
-//     appendedTdName.appendChild(nameInputDiv);
-//     const appendedNameInputDiv = document.getElementById(nameInputDiv.id);
-//     appendedNameInputDiv.appendChild(nameInputBox);
-
-//     newAppendedTr.appendChild(tdEmail);
-//     document.getElementById(tdEmail.id).setAttribute( "onClick", "turnEditOn(" + record.id + ")");
-//     const appendedTdEmail = document.getElementById(tdEmail.id);
-//     appendedTdEmail.appendChild(emailText);
-//     appendedTdEmail.appendChild(emailInputDiv);
-//     const appendedEmailInputDiv = document.getElementById(emailInputDiv.id);
-//     appendedEmailInputDiv.appendChild(emailInputBox);
-
-
-//     newAppendedTr.appendChild(tdSaveBtn);
-//     tdSaveBtn.appendChild(btnSave);
-//     document.getElementById("save-btn" + record.id).setAttribute( "onClick", "submitEditRec(" + record.id + ")");
-    
-//     newAppendedTr.appendChild(tdCancelBtn);
-//     tdCancelBtn.appendChild(btnCancel);
-//     document.getElementById(btnCancel.id).setAttribute( "onClick", "turnEditOff(" + record.id + ")");    
-
-//     newAppendedTr.appendChild(tdDelBtn);
-//     tdDelBtn.appendChild(btnDelete);
-//     document.getElementById(btnDelete.id).setAttribute( "onClick", "sendDELETEequest(" + record.id + ")");
-    
-// }
-
+/****************************
+ * SUBMITS (ADDS) NEW RECORD
+ *  FROM HTML TO THE DATA
+ ****************************/
 function submitNewRec(e) {
     e.preventDefault();
     console.log('new rec submit!!');
+    // Window.alert("FDF");
 
     const nameToSubmit = document.getElementById("new-name").value;
     const emailToSubmit = document.getElementById("new-email").value;
@@ -272,6 +185,10 @@ function submitNewRec(e) {
     sendPOSTRequest(serverURL, submissionObj);
 }
 
+/*********************************
+ * CALLS FETCH-PUT TO UPDATE
+ *  A RECORD WITH NEW NAME, EMAIL
+ ********************************/
 function submitEditRec(id) {
     // e.preventDefault();
     console.log('edit rec submit!!! id=', id);
@@ -289,24 +206,11 @@ function submitEditRec(id) {
     sendPUTRequest(serverURL + "/" + id, submissionObj);
 }
 
-
-// function toggleEdit(lineID) {
-//     console.log('edit button clicked');
-//     document.getElementById("name-text" + lineID).hidden = !document.getElementById("name-text" + lineID).hidden;
-//     document.getElementById("name-input" + lineID).hidden = !document.getElementById("name-input" + lineID).hidden;
-//     document.getElementById("email-text" + lineID).hidden = !document.getElementById("email-text" + lineID).hidden;
-//     document.getElementById("email-input" + lineID).hidden = !document.getElementById("email-input" + lineID).hidden;
-//     document.getElementById("save-btn" + lineID).hidden = !document.getElementById("save-btn" + lineID).hidden;
-//     // document.getElementById("save-btn" + lineID).disabled = !document.getElementById("save-btn" + lineID).disabled;
-    
-//     const editCancelBtn = document.getElementById("edit-btn" + lineID);
-//     if (editCancelBtn.innerHTML == "Edit") {
-//         editCancelBtn.innerHTML = "Cancel";
-//     } else {
-//         editCancelBtn.innerHTML = "Edit";
-//     }
-// }
-
+/*********************************
+ * WHEN RECORD CLICKED - 
+ * HIDES THE TEXT-BOXES AND
+ * UNHIDES THE INPUT-BOXES
+ ********************************/
 function turnEditOn(lineID) {
     console.log('turnEditOn');
 
@@ -326,6 +230,11 @@ function turnEditOn(lineID) {
     editOpenID = lineID;
 }
 
+/*********************************
+ * WHEN OTEHR RECORD CLICKED - 
+ * UNHIDES THE TEXT-BOXES AND
+ * HIDES THE INPUT-BOXES
+ ********************************/
 function turnEditOff(lineID) {
     console.log('turnEditOff');
 
@@ -340,52 +249,42 @@ function turnEditOff(lineID) {
     isEditOn = false;
     editOpenID = 0;
 }
-
+/*********************************
+ * TOGGLES HIDE/UNHIDE 
+ * OF THE ADD NEW RECORD INPUTS
+ * AND BOX
+ ********************************/
 function showHideAddNew() {
     if (document.getElementById("name-input-new").hidden) {
         console.log('show new');
         document.getElementById("name-input-new").hidden = false;
         document.getElementById("email-input-new").hidden = false;
-        document.getElementById("submit-btn").hidden = false;
+        document.getElementById("submit-form-btn").hidden = false;
         document.getElementById("toggle-add-rec").innerHTML = "X";
     } else {
         console.log('hide new');
         document.getElementById("name-input-new").hidden = true;
         document.getElementById("email-input-new").hidden = true;
-        document.getElementById("submit-btn").hidden = true;
+        document.getElementById("submit-form-btn").hidden = true;
         document.getElementById("toggle-add-rec").innerHTML = "+";
     }
 }
 
 
-function editNew1() {
-    console.log('new editttt');
-}
-
-function myFunc () {
-    console.log('my func');
-}
-
 function init() {
+    /* adds only one record, not from data, for debugging */
+    // addRecordToPage({id: 111, name: "Eli", email: "@#$"});
     getData(serverURL);
-    console.log('after getData');
+    console.log('after getData');        
+}
 
-    const newRecForm = document.getElementById("add-record");
-    // newRecForm.addEventListener("submit", submitNewRec);
-    console.log(newRecForm.addEventListener("submit", myFunc));
-    
-    
-    // document.getElementById("submit-btn").addEventListener("onclick", myFunc);
+function initAfterApendData() {
+    console.log('init after append data');
 
     
-    
-    
-
-    // const myForm = document.getElementById("my-form");
-    
-    // myForm.addEventListener("submit", formSubmit);
 }
 
 window.onload = () => {
     init();
 }
+
